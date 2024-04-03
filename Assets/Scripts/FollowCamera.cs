ddbc14b38dedefebd94c447b7312e52ba429de9b
Andrew <andrew@Capelagames.com.au>
2024-04-03 13:48:29 +1100

    character controller

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Update = UnityEngine.PlayerLoop.Update;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform follow;
+    public float cameraHeight = 5f;
+    public Vector2 cameraOffset;
+
+    private Vector3 focus;
+
+    private void Start()
+    {
+        focus = follow.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 offset = new Vector3(cameraOffset.x, 0f, cameraOffset.y);
+        transform.position = focus + (Vector3.up * cameraHeight) + offset;
+        transform.LookAt(focus);
+        FocusUpdate();
+    }
+
+    private Vector3 velocity;
+    private void FocusUpdate()
+    {
+        if (Vector3.Distance(focus, transform.position) > 5f)
+        {
+            focus = Vector3.SmoothDamp(focus, follow.position, ref velocity, 1f);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Vector3 input;
+    private bool isGrounded;
+    
+    public Camera camera;
     public float speed = 5f;
+    public LayerMask groundMask;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (camera == null)
+        {
+            camera = Camera.main;     
+        }
+    }
     void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"),
-            0, 
-            Input.GetAxis("Vertical"));
-
-        if (input.magnitude > 1)
+        //Input
+        Vector3 vertInput = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        Vector3 horiInput = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        
+        /*if (input.magnitude > 1)
         {
             input.Normalize();
+        }*/
+        
+        //Relational Movement
+        vertInput = camera.transform.TransformDirection(vertInput);
+        horiInput = camera.transform.TransformDirection(horiInput);
+        input = vertInput + horiInput;
+        input.y = 0f;
+        input.Normalize();
+        
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
         }
+        
+    }
 
-        transform.position += input * speed * Time.deltaTime;
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + input * (speed * Time.deltaTime));
+
+        isGrounded = Physics.Raycast(transform.position, -transform.up, 1.05f, groundMask);
+        //Debug.DrawRay(transform.position,-transform.up * 1.05f,Color.magenta);
     }
 }
