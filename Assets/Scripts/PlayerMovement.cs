cc076ec8017ea81907d58ed2c6f873dfccf8c7e8
RiftVoid <159392203+RiftVoid@users.noreply.github.com>
2024-04-03 15:10:50 +1100

    dying

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,20 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Vector3 input;
+    private bool isGrounded;
+
+    public Camera camera;
+
     public float speed = 5f;
+    public LayerMask groundMask;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+
+                
+    }
     void Update()
     {
-        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 
+        input = new Vector3(Input.GetAxis("Horizontal"), 
                                     0f,
                                     Input.GetAxis("Vertical"));
+        float inputMag = input.magnitude;
+        input = camera.transform.TransformDirection(input);
+        input.y = 0f;
+        input.Normalize();
+        input *= inputMag;
+
         if (input.magnitude > 1)
         {
             input.Normalize();
         }
-        
-        transform.position += input * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up*5f,ForceMode.VelocityChange);
+        }
     }
+ private void FixedUpdate()
+   {
+        
+    
+    rb.MovePosition(transform.position + input * speed * Time.deltaTime);
+
+    isGrounded = Physics.Raycast(transform.position, -transform.up, 1.05f,groundMask);
+
+   }
 }
+
